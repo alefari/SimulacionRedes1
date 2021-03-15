c5c955dc13fd612a2ede5234664212a63f1612b9
alefari <ale.jf09@gmail.com>
2021-03-15 16:48:50 -0400

    acomode detalles menores

--- proyectoModelacion.py
+++ proyectoModelacion.py
@@ -36,22 +36,15 @@ matrizAdyacencia = [[0,    40,  35,  60, 180,   0,   0, 150, 180,   0,   0],
                     [0,	    0,   0,   0,   0,   0,   0,  75,   0,	0,	 0]]
 
 matrizAdyacenciaMenorDistancia = [[0,   1,   1,   1,   1,   0,   0,   1,   1,   0,   0],
-                                  [1,   0,   1,   1,   0,   1,
-                                      0,   0,   0,	0,	 0],
+                                  [1,   0,   1,   1,   0,   1,   0,   0,   0,	0,	 0],
                                   [1,   1,   0,   1,   0,   1,	 0,   0,   0,	0,	 0],
-                                  [1,   1,   1,   0,   0,   0,
-                                      0,   0,   0,   0,	 0],
-                                  [1,   0,   0,   0,   0,   1,
-                                      0,   0,   0,   0,   0],
-                                  [0,   1,   1,   0,   1,	0,
-                                      1,   1,   1,   1,   0],
-                                  [0,   0,   0,   0,   0,   1,
-                                      0,   0,   0,   1,   0],
-                                  [1,   0,   0,   0,   0,   1,
-                                      0,   0,   1,   1,   1],
+                                  [1,   1,   1,   0,   0,   0,   0,   0,   0,   0,	 0],
+                                  [1,   0,   0,   0,   0,   1,   0,   0,   0,   0,   0],
+                                  [0,   1,   1,   0,   1,	0,   1,   1,   1,   1,   0],
+                                  [0,   0,   0,   0,   0,   1,   0,   0,   0,   1,   0],
+                                  [1,   0,   0,   0,   0,   1,   0,   0,   1,   1,   1],
                                   [1,   0,   0,   0,   0,   1,	 0,   1,   0,	0,	 0],
-                                  [0,   0,   0,   0,   0,   1,
-                                      1,   1,   0,	0,	 0],
+                                  [0,   0,   0,   0,   0,   1,   1,   1,   0,	0,	 0],
                                   [0,	0,   0,   0,   0,   0,   0,   1,   0,	0,	 0]]
 
 #     CODIGO, DIST MIN DESDE INICIO, PREDECESOR, VISITADO, REQUIERE VISA
@@ -108,7 +101,6 @@ def calcularRuta():
     while(esteNodo != origen):
         esteNodo = matrizNodos[esteNodo][2]
         cadena = matrizNodos[esteNodo][0] + ' - ' + cadena
-        print(matrizNodos)
     print('******************** RESULTADOS ********************')
     print('')
     if(option == '1'):
@@ -130,14 +122,17 @@ def calcularRuta():
         print('COSTO TOTAL: $', matrizNodos[destino][1])
 
 # INICIO DE LA EJECUCION DEL PROGRAMA
-print('Códigos: CCS	AUA	CUR	BON	SDQ	SXM	SBH	POS	BGI	PTP	FDF')
-print('Ingrese \n (1) Si desea obtener la ruta mas corta \n (2) Si desea obtener la ruta con el menor costo')
+
+print('Bienvenido, ingrese \n (1) Si desea obtener la ruta mas corta \n (2) Si desea obtener la ruta con el menor costo')
 option = input()
 while(not(option == '1' or option == '2')):
     print('ERROR EN OPCIÓN INGRESADA, vuelva a ingresar la opción:')
     option = input()
 if(option == '1'):
     matrizAdyacencia = matrizAdyacenciaMenorDistancia
+elif(option == '2'):
+    matrizAdyacencia = matrizAdyacenciaOriginal
+
 print('Ingrese el código del aeropuerto origen:')
 origen = input().upper()
 while(not(origen in DiccionarioIndices)):
@@ -152,5 +147,7 @@ while(not(destino in DiccionarioIndices)):
     print('ERROR EN CÓDIGO INGRESADO, vuelva a ingresar el código del aeropuerto destino:')
     destino = input().upper()
 destino = DiccionarioIndices[destino]
+
+
 dijkstra()
 calcularRuta()
